f2ee31eb1983bddb4532f5f793bc4937aae9c91b
SengSerany <cranyseng@gmail.com>
2019-08-06 20:18:48 +0200

    Initial commit

---
+++ app/controllers/gossips_controller.rb
@@ -0,0 +1,16 @@
+class GossipsController < ApplicationController
+  def index
+  	@gossips = Gossip.all
+  end
+
+  def create
+  	@user = User.create(first_name: params[:gossip_user])
+  	@gossip = Gossip.create(content: params[:gossip_content], user_id: @user.id, title: params[:gossip_title])
+  		if @gossip.save
+  			redirect_to gossips_path
+  		else
+  			flash[:
+  			render :new, error: "Invalid title or content"
+  		end
+  end
+end
