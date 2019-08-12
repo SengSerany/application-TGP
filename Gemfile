907dd6173338a93e3f772be2031203b3b4b4b15f
SengSerany <cranyseng@gmail.com>
2019-08-12 11:50:47 +0200

    add welcome_email

--- Gemfile
+++ Gemfile
@@ -3,6 +3,9 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.5.1'
 
+gem 'dotenv-rails'
+gem 'letter_opener'
+gem 'better_errors'
 gem 'pry'
 gem 'bcrypt'
 gem 'faker'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,14 @@
 class User < ApplicationRecord
+after_create :welcome_send
+
 	has_secure_password
 	belongs_to :city
 	has_many :comments
 	has_many :gossips
 	validates :password,
 		presence: true
+
+	def welcome_send
+    UserMailer.welcome_email(self).deliver_now
+  end
 end
